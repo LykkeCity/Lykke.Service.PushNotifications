e1e705539ea68e54015393f375d3ec5301698e6e
atarutin <tarutin_andrew@mail.ru>
2017-09-27 21:04:49 +0300

    removed Log from extension method

--- client/Lykke.Service.PushNotifications.Client/AutofacExtension.cs
+++ client/Lykke.Service.PushNotifications.Client/AutofacExtension.cs
@@ -6,11 +6,10 @@ namespace Lykke.Service.PushNotifications.Client
 {
     public static class AutofacExtension
     {
-        public static void RegisterPushNotificationsClient(this ContainerBuilder builder, string serviceUrl, ILog log)
+        public static void RegisterPushNotificationsClient(this ContainerBuilder builder, string serviceUrl)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
             if (serviceUrl == null) throw new ArgumentNullException(nameof(serviceUrl));
-            if (log == null) throw new ArgumentNullException(nameof(log));
             if (string.IsNullOrWhiteSpace(serviceUrl))
                 throw new ArgumentException("Value cannot be null or whitespace.", nameof(serviceUrl));
 
